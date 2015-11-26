d5d06edcf209b768f6f516a15858c405b3b3a7ad
Dennis Günnewig <dg1@ratiodata.de>
2015-11-26 08:46:13 +0100

    Added some more documentation

--- lib/event/bus.rb
+++ lib/event/bus.rb
@@ -8,13 +8,29 @@ module Event
   # to subscribe to different events that fire as your tests are executed.
   #
   class Bus
+    # Create Event::Bus
+    #
+    # @param [#transform] resolver
+    #   A resolver which transforms Symbol, String, Class into an event Class.
     def initialize(resolver)
       @resolver = resolver
       @handlers = Hash.new { |h, k| h[k] = [] }
     end
 
     # Register for an event
-    def register(event_id, handler_object = nil, &handler_proc)
+    # 
+    # @param [String, Symbol, Class, Array] event_ids
+    #   If Array, register multiple events witht the same handler. If String,
+    #   Symbol, Class register handler for given event.
+    #
+    # @param [#call] handler_object
+    #   The handler object, needs to have method `#call`. Either
+    #   `handler_object` or `block` can be defined. The handler object gets the
+    #   event passed to `#call`.
+    #
+    # @yield
+    #   Handler block which gets the event passed as parameter.
+    def register(event_ids, handler_object = nil, &handler_proc)
       handler = handler_proc || handler_object
 
       fail ArgumentError, 'Please pass either an object#call or a handler block' if handler.nil? || !handler.respond_to?(:call)
@@ -30,6 +46,11 @@ module Event
     end
 
     # Broadcast an event
+    #
+    # @param [Object] event
+    #   An object of registered event class. This object is passed to the event
+    #   handler.
+    #
     def notify(event)
       fail NoEventError, 'Please pass an event object, not a class' if event.is_a?(Class)
 
